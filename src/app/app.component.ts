5b55e9db583a3fab2a69d3fbffff162aecfcd773
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-05-28 19:01:58 +0200

    using jsonp to get the most popular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MoviesService } from './services/movies.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'movies-app';
+  constructor(public moviesService: MoviesService) {
+ 
+    this.moviesService.getMostPopular().subscribe(data => {
+        console.log(data);
+    });
+
+}
 }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
 
-  constructor() { }
+  private apiKey = 'MY_API_KEY';
+  private urlMoviedb = 'https://api.themoviedb.org/3';
+  
+  constructor(private http: HttpClient) { }
+
+  getMostPopular(){
+
+      let url = `${ this.urlMoviedb }/discover/movie?sort_by=popularity.desc&api_key=${ this.apiKey }&language=es&callback=JSONP_CALLBACK`;
+
+      return this.http.jsonp(url,'JSONP_CALLBACK');
+
+  }
 }
